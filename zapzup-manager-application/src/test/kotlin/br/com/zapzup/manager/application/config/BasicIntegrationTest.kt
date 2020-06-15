c83612022002b56dacecbfa2dc47f22ffd800080
Lihgp <guimaraesdpa@gmail.com>
2020-06-15 18:51:37 -0300

    fix

--- zapzup-manager-application/src/test/kotlin/br/com/zapzup/manager/application/config/BasicIntegrationTest.kt
+++ zapzup-manager-application/src/test/kotlin/br/com/zapzup/manager/application/config/BasicIntegrationTest.kt
@@ -1,7 +1,6 @@
 package br.com.zapzup.manager.application.config
 
 import org.junit.Before
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
