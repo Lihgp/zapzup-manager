ee78a0b1ed56ab4478ac7d7de9ff97eece92e290
SantosVictor1 <victor.udi10@hotmail.com>
2020-06-22 20:11:16 -0300

    Added todo

--- zapzup-manager-service/src/main/kotlin/br/com/zapzup/manager/service/user/impl/UserService.kt
+++ zapzup-manager-service/src/main/kotlin/br/com/zapzup/manager/service/user/impl/UserService.kt
@@ -18,6 +18,7 @@ class UserService(
     override fun create(createUserTO: CreateUserTO): UserTO {
         val encryptedPassword = passwordEncoder.encode(createUserTO.password)
         val user = createUserTO.toEntity().copy(password = encryptedPassword)
+        TODO("TRATAR EXCEPTIONS")
 
         return userRepository.save(user).toTO()
     }
