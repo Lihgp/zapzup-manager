5fb55beb07fa263d2574202d4ec5b87795205132
SantosVictor1 <victor.udi10@hotmail.com>
2020-06-22 18:09:46 -0300

    Created create contract into user controller and user service interface

--- zapzup-manager-api/src/main/kotlin/br/com/zapzup/manager/api/user/UserApi.kt
+++ zapzup-manager-api/src/main/kotlin/br/com/zapzup/manager/api/user/UserApi.kt
@@ -1,4 +1,21 @@
 package br.com.zapzup.manager.api.user
 
+import br.com.zapzup.manager.api.ResponseWrapper
+import br.com.zapzup.manager.api.user.request.UserRequest
+import br.com.zapzup.manager.api.user.response.UserResponse
+import org.springframework.http.HttpStatus.CREATED
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@RequestMapping(value = ["/user"])
 interface UserApi {
+
+    @PostMapping
+    @ResponseStatus(CREATED)
+    @ResponseBody
+    fun create(@RequestBody @Validated userRequest: UserRequest): ResponseWrapper<UserResponse>
 }
--- zapzup-manager-service/src/main/kotlin/br/com/zapzup/manager/service/test/user/IUserService.kt
+++ zapzup-manager-service/src/main/kotlin/br/com/zapzup/manager/service/test/user/IUserService.kt
@@ -1,4 +1,8 @@
 package br.com.zapzup.manager.service.test.user
 
+import br.com.zapzup.manager.domain.to.UserTO
+
 interface IUserService {
+
+    fun create(userTO: UserTO): UserTO
 }
