5a4357a4260e3ed2ec0526252771a4bbe6b34805
SantosVictor1 <victor.udi10@hotmail.com>
2020-06-22 18:25:58 -0300

    Created attributes for UserRequest

--- zapzup-manager-api/src/main/kotlin/br/com/zapzup/manager/api/user/request/UserRequest.kt
+++ zapzup-manager-api/src/main/kotlin/br/com/zapzup/manager/api/user/request/UserRequest.kt
@@ -1,4 +1,14 @@
 package br.com.zapzup.manager.api.user.request
 
-class UserRequest {
-}
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotEmpty
+
+data class UserRequest(
+    @field:[NotEmpty]
+    val username: String,
+    @field:[NotEmpty]
+    val email: String,
+    @field:[NotEmpty]
+    @field:[Min(8)]
+    val password: String
+)
