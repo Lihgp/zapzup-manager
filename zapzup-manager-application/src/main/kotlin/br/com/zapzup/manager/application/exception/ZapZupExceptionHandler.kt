47992faf96ffbc7d20a5fec39170769bf0f7a836
SantosVictor1 <victor.udi10@hotmail.com>
2020-06-23 12:46:51 -0300

    Created exception handler

--- zapzup-manager-application/src/main/kotlin/br/com/zapzup/manager/application/exception/ZapZupExceptionHandler.kt
+++ zapzup-manager-application/src/main/kotlin/br/com/zapzup/manager/application/exception/ZapZupExceptionHandler.kt
@@ -1,4 +1,71 @@
 package br.com.zapzup.manager.application.exception
 
-class ZapZupExceptionHandler {
+import br.com.zapzup.manager.infrastructure.error.ErrorResponse
+import br.com.zapzup.manager.infrastructure.error.FieldsErrorCode
+import br.com.zapzup.manager.infrastructure.error.ZapZupErrorCode
+import org.apache.logging.log4j.LogManager
+import org.springframework.context.MessageSource
+import org.springframework.context.i18n.LocaleContextHolder
+import org.springframework.http.HttpStatus.BAD_REQUEST
+import org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.ResponseStatus
+import java.util.LinkedList
+
+@ControllerAdvice
+class ZapZupExceptionHandler(
+    private val messageSource: MessageSource
+) {
+
+    private val log = LogManager.getLogger(this.javaClass)
+
+    @ExceptionHandler(Exception::class)
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    fun genericException(ex: Exception): ErrorResponse {
+        val code = ZapZupErrorCode.GENERAL_ERROR.code
+        val originalError = ex.javaClass.name + " - " + ex.message
+        val message = getMessage(ZapZupErrorCode.GENERAL_ERROR.key)
+
+        log.error("Exception: {}", originalError, ex)
+
+        return ErrorResponse(
+            code = code, message = message ?: code, originalError = originalError
+        )
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    @ResponseStatus(BAD_REQUEST)
+    @ResponseBody
+    fun handleMethodArgumentNotValidException(ex: MethodArgumentNotValidException): ErrorResponse {
+        val fields = LinkedHashMap<String, List<String>>()
+
+        log.error("MethodArgumentNotValidException ", ex)
+
+        ex.bindingResult.fieldErrors.forEach { field ->
+            fields.computeIfAbsent(field.field) {
+                LinkedList<String>().also { f ->
+                    when (field.code) {
+                        ("NotNull") -> f.add(FieldsErrorCode.MUST_NOT_BE_NULL.code)
+                        ("NotBlank") -> f.add(FieldsErrorCode.MUST_NOT_BE_BLANK.code)
+                        ("NotEmpty") -> f.add(FieldsErrorCode.MUST_NOT_BE_EMPTY.code)
+                        ("Size") -> f.add(FieldsErrorCode.INCORRECT_SIZE.code)
+                        else -> f.add(FieldsErrorCode.METHOD_ARGUMENT_INVALID.code)
+                    }
+                }
+            }
+        }
+
+        return ErrorResponse(
+            fields = fields,
+            message = getMessage(ZapZupErrorCode.METHOD_ARGUMENT_INVALID.key)
+                ?: ZapZupErrorCode.METHOD_ARGUMENT_INVALID.code,
+            code = ZapZupErrorCode.METHOD_ARGUMENT_INVALID.code
+        )
+    }
+
+    private fun getMessage(key: String): String? = messageSource.getMessage(key, null, LocaleContextHolder.getLocale())
 }
