a835364357ee06e5f1256c4032c002b41447b8f7
matheusfariazup <matheus.faria@zup.com.br>
2020-06-22 21:16:29 -0300

    Get User controller creation - with error

--- zapzup-manager-api/src/main/kotlin/br/com/zapzup/manager/api/user/UserApi.kt
+++ zapzup-manager-api/src/main/kotlin/br/com/zapzup/manager/api/user/UserApi.kt
@@ -1,4 +1,23 @@
 package br.com.zapzup.manager.api.user
 
+import br.com.zapzup.manager.api.ResponseWrapper
+import br.com.zapzup.manager.api.user.response.UserResponse
+import org.springframework.http.HttpStatus.OK
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.ResponseStatus
+
 interface UserApi {
+
+    @GetMapping
+    @ResponseBody
+    @ResponseStatus(OK)
+    fun getUsers(
+        @RequestParam(name = "email", required = false) email: String,
+        @RequestParam(name = "username", required = false) username: String,
+        @RequestParam(name = "name", required = false) name: String,
+        @RequestParam(name = "page", defaultValue = "1", required = false) page: Int,
+        @RequestParam(name = "limit", defaultValue = "10", required = false) limit: Int
+    ): ResponseWrapper<List<UserResponse>>
 }
--- zapzup-manager-application/src/main/kotlin/br/com/zapzup/manager/application/controller/user/UserController.kt
+++ zapzup-manager-application/src/main/kotlin/br/com/zapzup/manager/application/controller/user/UserController.kt
@@ -1,4 +1,32 @@
 package br.com.zapzup.manager.application.controller.user
 
-class UserController {
+import br.com.zapzup.manager.api.ResponseWrapper
+import br.com.zapzup.manager.api.user.UserApi
+import br.com.zapzup.manager.api.user.response.UserResponse
+import br.com.zapzup.manager.domain.to.user.GetUsersFilter
+import br.com.zapzup.manager.service.user.impl.UserService
+import org.springframework.web.bind.annotation.RequestParam
+
+class UserController (
+    private val userService: UserService
+) : UserApi {
+
+    override fun getUsers(
+        @RequestParam(name = "email", required = false) email: String,
+        @RequestParam(name = "username", required = false) username: String,
+        @RequestParam(name = "name", required = false) name: String,
+        @RequestParam(name = "page", defaultValue = "1", required = false) page: Int,
+        @RequestParam(name = "limit", defaultValue = "10", required = false) limit: Int
+    ): ResponseWrapper<List<UserResponse>> {
+
+        val filter = GetUsersFilter(
+            email = email,
+            username = username,
+            name = name,
+            page = page,
+            limit = limit
+        )
+
+        return ResponseWrapper(userService.getUsers(filter))
+    }
 }
