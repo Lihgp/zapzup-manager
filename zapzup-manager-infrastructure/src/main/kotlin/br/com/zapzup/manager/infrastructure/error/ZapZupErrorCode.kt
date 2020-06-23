568eed910f3e4cf5a15e7e80a38d7ca82258233a
SantosVictor1 <victor.udi10@hotmail.com>
2020-06-23 12:34:27 -0300

    Created UserNotFoundException

--- zapzup-manager-infrastructure/src/main/kotlin/br/com/zapzup/manager/infrastructure/error/ZapZupErrorCode.kt
+++ zapzup-manager-infrastructure/src/main/kotlin/br/com/zapzup/manager/infrastructure/error/ZapZupErrorCode.kt
@@ -2,7 +2,7 @@ package br.com.zapzup.manager.infrastructure.error
 
 class GeneralError(code: String, key: String) : ErrorCode(code, key)
 
-object ZZAErrorCode {
+object ZapZupErrorCode {
     val GENERAL_ERROR: ErrorCode = GeneralError("GENERAL_ERROR", "general.error")
     val METHOD_ARGUMENT_INVALID: ErrorCode = GeneralError("METHOD_ARGUMENT_INVALID", "method.argument.invalid")
 }
