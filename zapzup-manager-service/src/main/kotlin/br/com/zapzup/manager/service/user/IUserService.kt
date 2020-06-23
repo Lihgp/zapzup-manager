941c3d4b2cad2e5639d65de68a6acca1f4312dbb
matheusfariazup <matheus.faria@zup.com.br>
2020-06-22 21:18:14 -0300

    Get User service creation

--- zapzup-manager-service/src/main/kotlin/br/com/zapzup/manager/service/user/IUserService.kt
+++ zapzup-manager-service/src/main/kotlin/br/com/zapzup/manager/service/user/IUserService.kt
@@ -1,4 +1,9 @@
 package br.com.zapzup.manager.service.user
 
+import br.com.zapzup.manager.domain.to.user.GetUsersFilter
+import br.com.zapzup.manager.domain.to.user.UserTO
+
 interface IUserService {
+
+    fun getUsers(filter: GetUsersFilter): List<UserTO>
 }
--- zapzup-manager-service/src/main/kotlin/br/com/zapzup/manager/service/user/impl/UserService.kt
+++ zapzup-manager-service/src/main/kotlin/br/com/zapzup/manager/service/user/impl/UserService.kt
@@ -1,4 +1,34 @@
 package br.com.zapzup.manager.service.user.impl
 
-class UserService {
+import br.com.zapzup.manager.domain.to.user.GetUsersFilter
+import br.com.zapzup.manager.domain.to.user.UserTO
+import br.com.zapzup.manager.repository.UserRepository
+import br.com.zapzup.manager.service.user.IUserService
+import br.com.zapzup.manager.service.user.UserValidations.assertAndExtractParams
+
+class UserService(
+    private val userRepository: UserRepository
+): IUserService {
+
+    override fun getUsers(filter: GetUsersFilter): List<UserTO> {
+        val validatedFilter = assertAndExtractParams(filter)
+
+        val offset = (validatedFilter.page - 1) * validatedFilter.limit
+
+        val records = userRepository.findByQueryParams(
+            validatedFilter.email,
+            validatedFilter.name,
+            validatedFilter.username,
+            offset,
+            validatedFilter.limit
+        )
+
+        return if (records.isNotEmpty()) {
+            //TODO: Fazer o retorno caso tenha encotrado algum resultado. OBS: É necessário fazer a montagem
+            // das páginas
+            emptyList()
+        } else {
+            emptyList()
+        }
+    }
 }
