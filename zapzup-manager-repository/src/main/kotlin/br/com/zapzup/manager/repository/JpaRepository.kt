6f8dc89bdd764df2d48287baf3785497b1e0829b
matheusfariazup <matheus.faria@zup.com.br>
2020-06-22 21:19:02 -0300

    Get User repository creation - not finished

--- zapzup-manager-repository/src/main/kotlin/br/com/zapzup/manager/repository/JpaRepository.kt
+++ zapzup-manager-repository/src/main/kotlin/br/com/zapzup/manager/repository/JpaRepository.kt
@@ -3,4 +3,14 @@ package br.com.zapzup.manager.repository
 import br.com.zapzup.manager.domain.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserRepository : JpaRepository<User, String>
+interface UserRepository : JpaRepository<User, String> {
+
+    //TODO: Fazer a query. OBS: será query nativa?
+    fun findByQueryParams(
+        email: String,
+        name: String,
+        username: String,
+        offset: Int?,
+        limit: Int?
+    ): List<Any>
+}
