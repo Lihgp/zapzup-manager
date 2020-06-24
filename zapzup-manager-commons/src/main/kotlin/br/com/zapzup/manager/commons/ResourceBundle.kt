48d380c0cc191ce04f93f0beece4006ec01a130e
SantosVictor1 <victor.udi10@hotmail.com>
2020-06-24 20:22:46 -0300

    Created messages for email sending and resource bundle

--- zapzup-manager-commons/src/main/kotlin/br/com/zapzup/manager/commons/ResourceBundle.kt
+++ zapzup-manager-commons/src/main/kotlin/br/com/zapzup/manager/commons/ResourceBundle.kt
@@ -1,4 +1,14 @@
 package br.com.zapzup.manager.commons
 
-class ResourceBundle {
+import org.springframework.context.MessageSource
+import org.springframework.context.i18n.LocaleContextHolder
+import org.springframework.stereotype.Component
+
+@Component
+class ResourceBundle(
+    private val messageSource: MessageSource
+) {
+
+    fun getMessage(message: String, args: Array<String> = arrayOf()): String?
+        = messageSource.getMessage(message, args, LocaleContextHolder.getLocale())
 }
