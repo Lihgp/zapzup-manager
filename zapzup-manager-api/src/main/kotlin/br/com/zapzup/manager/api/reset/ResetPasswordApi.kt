aec683b7f72c53ede40d39d774f422e006267e8d
SantosVictor1 <victor.udi10@hotmail.com>
2020-06-25 12:33:15 -0300

    Created controller for token validation

--- zapzup-manager-api/src/main/kotlin/br/com/zapzup/manager/api/reset/ResetPasswordApi.kt
+++ zapzup-manager-api/src/main/kotlin/br/com/zapzup/manager/api/reset/ResetPasswordApi.kt
@@ -4,8 +4,10 @@ import br.com.zapzup.manager.api.reset.request.GenerateTokenRequest
 import io.swagger.annotations.Api
 import org.springframework.http.HttpStatus.ACCEPTED
 import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -18,4 +20,9 @@ interface ResetPasswordApi {
     @ResponseStatus(ACCEPTED)
     @ResponseBody
     fun generateResetToken(@RequestBody @Validated generateTokenRequest: GenerateTokenRequest)
+
+    @GetMapping(value = ["/validate"])
+    @ResponseStatus(ACCEPTED)
+    @ResponseBody
+    fun validateToken(@RequestHeader token: String)
 }
--- zapzup-manager-application/src/main/kotlin/br/com/zapzup/manager/application/controller/reset/ResetPasswordController.kt
+++ zapzup-manager-application/src/main/kotlin/br/com/zapzup/manager/application/controller/reset/ResetPasswordController.kt
@@ -2,12 +2,11 @@ package br.com.zapzup.manager.application.controller.reset
 
 import br.com.zapzup.manager.api.reset.ResetPasswordApi
 import br.com.zapzup.manager.api.reset.request.GenerateTokenRequest
-import br.com.zapzup.manager.service.email.IEmailService
 import br.com.zapzup.manager.service.reset.IResetPasswordService
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
-import java.util.UUID
 
 @RestController
 class ResetPasswordController(
@@ -16,5 +15,9 @@ class ResetPasswordController(
 
     override fun generateResetToken(@RequestBody @Validated generateTokenRequest: GenerateTokenRequest) {
         resetPasswordService.generateResetToken(generateTokenRequest.toDomain())
+    }
+
+    override fun validateToken(@RequestHeader token: String) {
+        resetPasswordService.validateToken(token)
     }
 }
