c5e586b2d0f94f80ca18c1ca060191920a4adcbf
SantosVictor1 <victor.udi10@hotmail.com>
2020-06-30 14:08:13 -0300

    Created orm.xml!

--- zapzup-manager-repository/src/main/kotlin/br/com/zapzup/manager/repository/JpaRepository.kt
+++ zapzup-manager-repository/src/main/kotlin/br/com/zapzup/manager/repository/JpaRepository.kt
@@ -3,13 +3,19 @@ package br.com.zapzup.manager.repository
 import br.com.zapzup.manager.domain.entity.Token
 import br.com.zapzup.manager.domain.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
 interface UserRepository : JpaRepository<User, String> {
-    fun existsByUsername(username: String): Boolean
-    fun existsByEmail(email: String): Boolean
-    fun findByEmail(email: String): User?
+    @Query(name = "UserEntity.existsByUsername", nativeQuery = true)
+    fun existsByUsername(@Param(value = "username") username: String): Boolean
+
+    @Query(name = "UserEntity.existsByEmail", nativeQuery = true)
+    fun existsByEmail(@Param(value = "email") email: String): Boolean
+
+    fun findByEmail(@Param(value = "email") email: String): User?
 }
 
 @Repository
