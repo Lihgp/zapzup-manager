50e0d3fb77a34fc02fa16a3e396c8b43da49d1cb
SantosVictor1 <victor.udi10@hotmail.com>
2020-07-03 12:30:05 -0300

    Added docker_username variable

--- build-image.sh
+++ build-image.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+DOCKER_USERNAME=$1
+
 docker-compose -f docker-compose.yml up -d
 mvn clean install
 docker-compose -f docker-compose.yml down
 cd zapzup-manager-application
-docker build -t victorsantoss/zapzup_manager:1.0 .
+docker build -t "$DOCKER_USERNAME"/zapzup_manager:1.0 .
 cd ../
 docker push victorsantoss/zapzup_manager:1.0
