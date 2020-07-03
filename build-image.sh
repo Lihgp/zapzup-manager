efba838324628a69335d6e0c90967eb78d31db9f
SantosVictor1 <victor.udi10@hotmail.com>
2020-07-03 12:36:27 -0300

    fix

--- build-image.sh
+++ build-image.sh
@@ -2,9 +2,9 @@
 
 DOCKER_USERNAME=$1
 
-docker-compose -f docker-compose.yml up -d
+docker-compose -f resource/docker-compose.yml up -d
 mvn clean install
-docker-compose -f docker-compose.yml down
+docker-compose -f resource/docker-compose.yml down
 cd zapzup-manager-application
 docker build -t "$DOCKER_USERNAME"/zapzup_manager:1.0 .
 cd ../
