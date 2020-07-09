fbfbe409024fa9223190163db6b55d791ebfacb7
SantosVictor1 <victor.udi10@hotmail.com>
2020-07-09 20:38:18 -0300

    Created and updated mapper

--- zapzup-manager-application/src/main/kotlin/br/com/zapzup/manager/application/controller/chat/ChatControllerMapper.kt
+++ zapzup-manager-application/src/main/kotlin/br/com/zapzup/manager/application/controller/chat/ChatControllerMapper.kt
@@ -1,2 +1,26 @@
 package br.com.zapzup.manager.application.controller.chat
 
+import br.com.zapzup.manager.api.chat.request.CreateChatRequest
+import br.com.zapzup.manager.api.chat.response.ChatResponse
+import br.com.zapzup.manager.application.controller.user.toResponseList
+import br.com.zapzup.manager.domain.to.chat.ChatTO
+import br.com.zapzup.manager.domain.to.chat.CreateChatTO
+
+fun CreateChatRequest.toDomain() = CreateChatTO(
+    userId = this.userId,
+    chatName = this.chatName
+)
+
+fun ChatTO.toResponse() = ChatResponse(
+    id = this.id,
+    name = this.name,
+    description = this.description,
+    status = this.status,
+    createdBy = this.createdBy,
+    updatedBy = this.updatedBy,
+    deletedBy = this.deletedBy,
+    createdAt = this.createdAt,
+    updatedAt = this.updatedAt,
+    deletedAt = this.deletedAt,
+    users = this.users.toResponseList()
+)
