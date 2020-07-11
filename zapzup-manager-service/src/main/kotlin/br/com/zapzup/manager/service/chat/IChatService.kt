569abb7faad9448f66920a40f576f85f7733fb9f
SantosVictor1 <victor.udi10@hotmail.com>
2020-07-11 12:12:47 -0300

    Created service for group chat creation

--- zapzup-manager-service/src/main/kotlin/br/com/zapzup/manager/service/chat/IChatService.kt
+++ zapzup-manager-service/src/main/kotlin/br/com/zapzup/manager/service/chat/IChatService.kt
@@ -2,8 +2,11 @@ package br.com.zapzup.manager.service.chat
 
 import br.com.zapzup.manager.domain.to.chat.ChatTO
 import br.com.zapzup.manager.domain.to.chat.CreateChatTO
+import br.com.zapzup.manager.domain.to.chat.CreateGroupChatTO
 
 interface IChatService {
 
     fun create(createChatTO: CreateChatTO): ChatTO
+
+    fun createGroupChat(createGroupChatTO: CreateGroupChatTO): ChatTO
 }
--- zapzup-manager-service/src/main/kotlin/br/com/zapzup/manager/service/chat/impl/ChatService.kt
+++ zapzup-manager-service/src/main/kotlin/br/com/zapzup/manager/service/chat/impl/ChatService.kt
@@ -1,8 +1,10 @@
 package br.com.zapzup.manager.service.chat.impl
 
 import br.com.zapzup.manager.domain.entity.Chat
+import br.com.zapzup.manager.domain.entity.User
 import br.com.zapzup.manager.domain.to.chat.ChatTO
 import br.com.zapzup.manager.domain.to.chat.CreateChatTO
+import br.com.zapzup.manager.domain.to.chat.CreateGroupChatTO
 import br.com.zapzup.manager.repository.ChatRepository
 import br.com.zapzup.manager.service.chat.IChatService
 import br.com.zapzup.manager.service.chat.mapper.toTO
@@ -16,12 +18,35 @@ class ChatService(
     private val userService: IUserService
 ) : IChatService {
 
-
     override fun create(createChatTO: CreateChatTO): ChatTO {
         val userTO = userService.getUserById(createChatTO.userId)
 
         val chat = chatRepository.save(
             Chat(createdBy = userTO.name, users = mutableListOf(userTO.toEntity()))
+        )
+
+        return chat.toTO()
+    }
+
+    override fun createGroupChat(createGroupChatTO: CreateGroupChatTO): ChatTO {
+        val members: MutableList<User> = mutableListOf()
+        val creatorUserTO = userService.getUserById(createGroupChatTO.creatorUserId)
+
+        members.add(creatorUserTO.toEntity())
+
+        createGroupChatTO.members.forEach {
+            val userTO = userService.getUserById(it.id)
+
+            members.add(userTO.toEntity())
+        }
+
+        val chat = chatRepository.save(
+            Chat(
+                name = createGroupChatTO.chatName,
+                description = createGroupChatTO.chatDescription,
+                createdBy = creatorUserTO.username,
+                users = members
+            )
         )
 
         return chat.toTO()
