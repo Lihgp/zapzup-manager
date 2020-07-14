92469dfdac17df5408ced0cb061bd6bf0066e251
SantosVictor1 <victor.udi10@hotmail.com>
2020-07-14 20:45:29 -0300

    Added websocket dependency and created config file

---
+++ zapzup-manager-application/src/main/kotlin/br/com/zapzup/manager/application/config/WebSocketConfig.kt
@@ -0,0 +1,22 @@
+package br.com.zapzup.manager.application.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+
+
+@Configuration
+@EnableWebSocketMessageBroker
+open class WebSocketConfig : WebSocketMessageBrokerConfigurer {
+
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        registry.addEndpoint("/messages").setAllowedOrigins("*").withSockJS()
+    }
+
+    override fun configureMessageBroker(registry: MessageBrokerRegistry) {
+        registry.setApplicationDestinationPrefixes("/app")
+        registry.enableSimpleBroker("/topic")
+    }
+}
